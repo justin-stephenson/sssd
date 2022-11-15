059a73ff7363eec6c4b711907964a4937fed3117
Justin Stephenson <jstephen@redhat.com>
2022-11-15 14:55:45 -0500

    Support Passkey prompting config

--- src/sss_client/sss_cli.h
+++ src/sss_client/sss_cli.h
@@ -525,6 +525,11 @@ enum response_type {
                             *   - verification_uri_complete
                             *   - user_code
                             */
+    SSS_PAM_PASSKEY_INFO, /**< Indicates that passkey authentication
+                            * is available, including a parameter string
+                            * which dictates whether prompting for PIN is
+                            * needed.
+                            * @param prompt_pin. */
 };
 
 /**
@@ -628,6 +633,7 @@ enum prompt_config_type {
     PC_TYPE_PASSWORD,
     PC_TYPE_2FA,
     PC_TYPE_2FA_SINGLE,
+    PC_TYPE_PASSKEY,
     PC_TYPE_SC_PIN,
     PC_TYPE_LAST
 };
@@ -639,6 +645,8 @@ const char *pc_get_password_prompt(struct prompt_config *pc);
 const char *pc_get_2fa_1st_prompt(struct prompt_config *pc);
 const char *pc_get_2fa_2nd_prompt(struct prompt_config *pc);
 const char *pc_get_2fa_single_prompt(struct prompt_config *pc);
+const char *pc_get_passkey_inter_prompt(struct prompt_config *pc);
+const char *pc_get_passkey_touch_prompt(struct prompt_config *pc);
 void pc_list_free(struct prompt_config **pc_list);
 errno_t pc_list_add_password(struct prompt_config ***pc_list,
                              const char *prompt);
@@ -646,6 +654,9 @@ errno_t pc_list_add_2fa(struct prompt_config ***pc_list,
                         const char *prompt_1st, const char *prompt_2nd);
 errno_t pc_list_add_2fa_single(struct prompt_config ***pc_list,
                                const char *prompt);
+errno_t pc_list_add_passkey(struct prompt_config ***pc_list,
+                            const char *inter_prompt,
+                            const char *touch_prompt);
 errno_t pam_get_response_prompt_config(struct prompt_config **pc_list, int *len,
                                        uint8_t **data);
 errno_t pc_list_from_response(int size, uint8_t *buf,
