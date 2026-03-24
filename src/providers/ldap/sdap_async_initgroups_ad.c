fbad3ad329a5d3cfe1c52e7da2faf0512229feaa
Justin Stephenson <jstephen@redhat.com>
2026-03-24 10:51:03 -0400

    DONOTMERGE: FIXME for AD provider
    This commit will be removed when AD provider is ported
    to new failover.

--- src/providers/ldap/sdap_async_initgroups_ad.c
+++ src/providers/ldap/sdap_async_initgroups_ad.c
@@ -447,7 +447,7 @@ sdap_ad_tokengroups_initgr_mapping_send(TALLOC_CTX *mem_ctx,
     struct tevent_req *req = NULL;
     struct tevent_req *subreq = NULL;
     struct sdap_domain *sdom;
-    struct ad_id_ctx *subdom_id_ctx;
+    //struct ad_id_ctx *subdom_id_ctx;
     errno_t ret;
 
     req = tevent_req_create(mem_ctx, &state,
@@ -485,6 +485,7 @@ sdap_ad_tokengroups_initgr_mapping_send(TALLOC_CTX *mem_ctx,
         }
     }
 
+    /*
     subdom_id_ctx = talloc_get_type(sdom->pvt, struct ad_id_ctx);
     state->op = sdap_id_op_create(state, subdom_id_ctx->ldap_ctx->conn_cache);
     if (!state->op) {
@@ -498,6 +499,7 @@ sdap_ad_tokengroups_initgr_mapping_send(TALLOC_CTX *mem_ctx,
         ret = ENOMEM;
         goto immediately;
     }
+    */
 
     tevent_req_set_callback(subreq,
                             sdap_ad_tokengroups_initgr_mapping_connect_done,
@@ -521,13 +523,14 @@ sdap_ad_tokengroups_initgr_mapping_connect_done(struct tevent_req *subreq)
 {
     struct sdap_ad_tokengroups_initgr_mapping_state *state = NULL;
     struct tevent_req *req = NULL;
-    int ret;
+    //int ret;
 
     req = tevent_req_callback_data(subreq, struct tevent_req);
     state = tevent_req_data(req,
                             struct sdap_ad_tokengroups_initgr_mapping_state);
 
 
+    /*
     ret = sdap_id_op_connect_recv(subreq);
     talloc_zfree(subreq);
 
@@ -535,9 +538,11 @@ sdap_ad_tokengroups_initgr_mapping_connect_done(struct tevent_req *subreq)
         tevent_req_error(req, ret);
         return;
     }
+    */
 
     subreq = sdap_get_ad_tokengroups_send(state, state->ev, state->opts,
-                                          sdap_id_op_handle(state->op),
+                                          // sdap_id_op_handle(state->op),
+                                          NULL,
                                           state->username,
                                           state->orig_dn, state->timeout);
     if (subreq == NULL) {
@@ -793,7 +798,7 @@ sdap_ad_tokengroups_initgr_posix_send(TALLOC_CTX *mem_ctx,
     struct tevent_req *req = NULL;
     struct tevent_req *subreq = NULL;
     struct sdap_domain *sdom;
-    struct ad_id_ctx *subdom_id_ctx;
+    //struct ad_id_ctx *subdom_id_ctx;
     errno_t ret;
 
     req = tevent_req_create(mem_ctx, &state,
@@ -831,7 +836,9 @@ sdap_ad_tokengroups_initgr_posix_send(TALLOC_CTX *mem_ctx,
             goto immediately;
         }
     }
+    /*
     subdom_id_ctx = talloc_get_type(sdom->pvt, struct ad_id_ctx);
+
     state->op = sdap_id_op_create(state, subdom_id_ctx->ldap_ctx->conn_cache);
     if (!state->op) {
         DEBUG(SSSDBG_OP_FAILURE, "sdap_id_op_create failed\n");
@@ -844,6 +851,7 @@ sdap_ad_tokengroups_initgr_posix_send(TALLOC_CTX *mem_ctx,
         ret = ENOMEM;
         goto immediately;
     }
+    */
 
     tevent_req_set_callback(subreq,
                             sdap_ad_tokengroups_initgr_posix_sids_connect_done,
@@ -867,13 +875,14 @@ sdap_ad_tokengroups_initgr_posix_sids_connect_done(struct tevent_req *subreq)
 {
     struct sdap_ad_tokengroups_initgr_posix_state *state = NULL;
     struct tevent_req *req = NULL;
-    int ret;
+    //int ret;
 
     req = tevent_req_callback_data(subreq, struct tevent_req);
     state = tevent_req_data(req,
                             struct sdap_ad_tokengroups_initgr_posix_state);
 
 
+/*
     ret = sdap_id_op_connect_recv(subreq);
     talloc_zfree(subreq);
 
@@ -881,9 +890,11 @@ sdap_ad_tokengroups_initgr_posix_sids_connect_done(struct tevent_req *subreq)
         tevent_req_error(req, ret);
         return;
     }
+    */
 
     subreq = sdap_get_ad_tokengroups_send(state, state->ev, state->opts,
-                                          sdap_id_op_handle(state->op),
+                                          // sdap_id_op_handle(state->op),
+                                          NULL,
                                           state->username, state->orig_dn,
                                           state->timeout);
     if (subreq == NULL) {
@@ -1207,6 +1218,7 @@ sdap_ad_get_domain_local_groups_send(TALLOC_CTX *mem_ctx,
         goto fail;
     }
 
+/*
     state->op = sdap_id_op_create(state, state->conn->conn_cache);
     if (state->op == NULL) {
         DEBUG(SSSDBG_OP_FAILURE, "sdap_id_op_create failed\n");
@@ -1219,7 +1231,7 @@ sdap_ad_get_domain_local_groups_send(TALLOC_CTX *mem_ctx,
         DEBUG(SSSDBG_OP_FAILURE, "sdap_id_op_connect_send failed.\n");
         goto fail;
     }
-
+*/
     tevent_req_set_callback(subreq,
                             sdap_ad_get_domain_local_groups_connect_done, req);
 
@@ -1239,8 +1251,8 @@ sdap_ad_get_domain_local_groups_connect_done(struct tevent_req *subreq)
                                                       struct tevent_req);
     struct sdap_ad_get_domain_local_groups_state *state = tevent_req_data(req,
                                   struct sdap_ad_get_domain_local_groups_state);
+/*
     int ret;
-
     ret = sdap_id_op_connect_recv(subreq);
     talloc_zfree(subreq);
 
@@ -1248,9 +1260,11 @@ sdap_ad_get_domain_local_groups_connect_done(struct tevent_req *subreq)
         tevent_req_error(req, ret);
         return;
     }
+    */
     subreq = rfc2307bis_nested_groups_send(state, state->ev, state->opts,
                                            state->sysdb, state->dom,
-                                           sdap_id_op_handle(state->op),
+                                           //sdap_id_op_handle(state->op),
+                                           NULL,
                                            state->search_bases,
                                            state->groups, state->num_groups,
                                            state->group_hash, 0);
